811cb93ab29c658d45d8c297998cc551b1e15f17
JZoom <jzoom8112@gmail.com>
2018-07-12 00:15:54 +0800

    pub

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,19 +23,14 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       _height = await FlutterStatusbar.height;
-    } on PlatformException {
-
-    }
+    } on PlatformException {}
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
-    if (!mounted)
-      return;
+    if (!mounted) return;
 
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
